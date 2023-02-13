d33bab8921603213a35b97dec917f9a51045566e
Bryan <ndjeutcha@gmail.com>
2023-02-13 13:46:33 -0500

    Ajouter preToken

--- auth/lsat.go
+++ auth/lsat.go
@@ -1,6 +1,17 @@
 package auth
 
+import "lsat/lightning"
+
 type Token struct {
 	mac       Macaroon
 	pre_image string
+}
+
+type preToken struct {
+	mac     Macaroon
+	invoice string
+}
+
+func (token Token) Pay(node lightning.LightningNode) Token {
+	return Token{}
 }
--- auth/mint.go
+++ auth/mint.go
@@ -12,15 +12,18 @@ type Service struct {
 	Price int64
 }
 
+type Ressource interface {
+}
+
 // Assure qu'un macaroon ait accès aux services
-type ServiveHandler interface {
+type ServiveManager interface {
 	Capabilities(context.Context, Service) ([]Caveat, error) // The capabilities of the service
 }
 
 // https://github.com/lightninglabs/aperture/blob/master/mint/mint.go#L65
 type Minter struct {
 	// Une abstraction des services offert par une application
-	service ServiveHandler
+	service ServiveManager
 
 	// La source des secrets des lsats qui seront créé
 	secrets secrets.SecretStore
@@ -28,4 +31,8 @@ type Minter struct {
 	// Crée les nouveaux invoices
 	// qui serviront de challenge pour créer les lsat
 	node lightning.LightningNode
+}
+
+func (minter Minter) mintToken(uid secrets.UserId, caveat []Caveat) (preToken, error) {
+	return preToken{}, nil
 }
